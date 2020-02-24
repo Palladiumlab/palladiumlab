59693f47dcf81679a22d40e3cd7d2b94010424f4
Ilya Sirenev <ilya@palladiumlab.com>
2020-02-24 21:40:29 +0300

    master // fix env loading

--- local/php_interface/load.php
+++ local/php_interface/load.php
@@ -1,6 +1,6 @@
 <?php
 if (!defined('ROOT_DIR')) {
-    define('ROOT_DIR', trim($_SERVER['DOCUMENT_ROOT'], '/'));
+    define('ROOT_DIR', $_SERVER['DOCUMENT_ROOT']);
 }
 if (file_exists(ROOT_DIR . '/.env')) {
     # Load .env file
